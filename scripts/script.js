09a58a38aa4e65f86135f1a401777fa1d43a1e69
samussd <101996753+samussd@users.noreply.github.com>
2023-04-09 06:46:03 -0300

    finish logic for 'expression' mode, improve UI

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,30 @@
 const displayTextTop = document.querySelector('.display__top-text');
 const displayTextBot = document.querySelector('.display__bot-text');
+const errorMsg = document.querySelector('.main__error-msg');
 
 const buttons = document.querySelectorAll('.button');
 
+let mode = 'quick';
 
 let savedNum = '';
 let operator = '';
 let inputNum = '';
+let expression = '';
 let equalOperatorDisplay = '';
 
-function handleInput(e) {
+/** Handles logic for the 'quick' mode; 
+ * You can only have one operation happening at a time on your display. 
+ * Every time an operator is pressed, it performs the current operation 
+ * and saves the result for the next one.
+*/
+function quickCalc(e) {
     let btn = e.target;
     let pressed = btn.dataset.value;
-    let type = btn.dataset.type;
     
+    //basic number input logic
     if (!isNaN(pressed)) {
-        if (!(pressed==='0' && (inputNum=='0' || !inputNum))) inputNum += pressed;
+        if (pressed!=='0' && inputNum==='0') inputNum = pressed;
+        else if (inputNum!=='0') inputNum += pressed;
         
     }
     else if (pressed === '.') {
@@ -23,18 +32,14 @@ function handleInput(e) {
 
     }
     else if (pressed === '+-') {
-        if (operator==='='){
-            inputNum = savedNum;
-            savedNum = '';
-            operator = '';
-        }
         if (inputNum.charAt(0)==='-') inputNum = inputNum.slice(1);
             else if (+inputNum!==0) inputNum = '-' + inputNum;
 
     }
+    //the sqrt, factorial and percent operations are performed directly on the input number
     else if (pressed === '√' && inputNum) {
         if (!savedNum) equalOperatorDisplay = `√${inputNum} =`;
-        inputNum = Math.sqrt(+inputNum).toString();
+        inputNum = getSqrt(+inputNum).toString();
 
     }
     else if (pressed === '!' && inputNum) {
@@ -42,34 +47,33 @@ function handleInput(e) {
         inputNum = factorial(+inputNum).toString();
 
     }
-    else if (pressed === '%' && inputNum) {
+    else if (pressed === '%' && inputNum) { // 50+10%=55; 50x10%=5
         if (!savedNum) {
             equalOperatorDisplay = `${inputNum}% =`;
             inputNum = (inputNum/100).toString();
         }
         else if (['×','*','+','-'].includes(operator)) {
             equalOperatorDisplay = `${savedNum} ${operator} ${inputNum}% =`;
-            if (operator==='-') inputNum = savedNum - (inputNum/100)*savedNum;
-            if (operator==='+') inputNum = savedNum + (inputNum/100)*savedNum;
-            if (operator==='×'||operator==='*') inputNum = (inputNum/100)*savedNum;
+            if (operator==='-') inputNum = +savedNum - (+inputNum/100)*+savedNum;
+            if (operator==='+') inputNum = +savedNum + (+inputNum/100)*+savedNum;
+            if (operator==='×'||operator==='*') inputNum = (+inputNum/100)*+savedNum;
         }
 
     }
+    //calculate expression and save it for the next operation, update operator
     else if (['^','×','/','+','-'].includes(pressed) && (savedNum || inputNum)) {
         if (savedNum && operator && inputNum) {
             savedNum = operate(+savedNum, +inputNum, operator).toString();
             inputNum = '';
         }
-        else if (operator) {
-            operator = pressed;
-        }
-        else {
+        else if (!operator){
             savedNum = inputNum;
             inputNum = '';
         }
         operator = pressed;
 
     }
+    //calculate expression and display result, reset operator
     else if (pressed === '=') {
         if (savedNum && operator && inputNum) {
             equalOperatorDisplay = `${savedNum} ${operator} ${inputNum} =`;
@@ -105,7 +109,141 @@ function handleInput(e) {
     }
 }
 
-buttons.forEach(n => n.addEventListener("click", e => handleInput(e)));
+/** Handles logic for the 'expression' mode; 
+ * You can type a longer expression with multiple operators, including parentheses. 
+ * The expression is evaluated once you click the '=' button.
+*/
+function expressionCalc(e) {
+    let btn = e.target;
+    let pressed = btn.dataset.value;
+    
+    if (!isNaN(pressed)) {
+        if (pressed!=='0' && inputNum==='0') inputNum = pressed;
+        else if (inputNum!=='0') inputNum += pressed;
+        
+    }
+    else if (pressed === '.') {;
+        if (!inputNum.includes('.')) inputNum += (inputNum) ? '.' : '0.';
+
+    }
+    else if (pressed === '+-') {
+        if (inputNum.charAt(0)==='-') inputNum = inputNum.slice(1);
+            else if (+inputNum!==0) inputNum = '-' + inputNum;
+
+    }
+    else if (pressed === '√') {
+        if (inputNum) {
+            expression += '√';
+            expression += inputNum;
+            inputNum = '';
+        }
+        else expression += '√';
+
+    }
+    else if (pressed === '!') {
+        if (inputNum) {
+            expression += inputNum;
+            expression += '!';
+            inputNum = '';
+        }
+        else expression += '!';
+
+    }
+    else if (pressed === '%') {
+        if (inputNum) {
+            expression += inputNum;
+            expression += '%';
+            inputNum = '';
+        }
+        else expression += '%';
+
+    }
+    else if (pressed === ')') {
+        expression += inputNum;
+        expression += pressed;
+        inputNum = '';
+    }
+    else if (pressed === '(') {
+        let lastChar = (expression+inputNum).slice(-1);
+        expression += inputNum;
+        if (inputNum && (['!','%',')'].includes(lastChar) || !isNaN(lastChar)) ) expression += '×';
+        expression += pressed;
+        inputNum = '';
+    }
+    else if (['^','×','/','+','-','('].includes(pressed)) {
+        let lastChar = (expression+inputNum).slice(-1);
+        if (!['^','×','/','+','-','('].includes(lastChar)) {
+            expression += inputNum;
+            expression += pressed;
+            inputNum = '';
+        }
+    }
+    else if (pressed === '=') {
+        expression += inputNum;
+        equalOperatorDisplay = `${beautifyExpression(expression)} =`
+        inputNum = solveExpression(expression.replace(/\s+/g, '')).toString();
+        expression = '';
+    }
+    else if (pressed === 'C') {
+        if (inputNum) inputNum = inputNum.slice(0,-1);
+        else expression = expression.slice(0,-1);
+
+    }
+    else if (pressed === 'CE') {
+        inputNum = '';
+        expression = '';
+    }
+
+
+    if (equalOperatorDisplay) {
+        displayTextTop.textContent = equalOperatorDisplay;
+        equalOperatorDisplay = '';
+    }
+    else {
+        displayTextTop.textContent = beautifyExpression(expression) + inputNum;
+    }
+    displayTextBot.textContent = (inputNum) ? inputNum : '0';
+}
+
+/** Add spaces around operators, unless it is a negation operator */
+const beautifyExpression = str => {
+    let result = '';
+    
+    for (let i = 0; i < str.length; i++) {
+        if (['^','×','/','+','-'].includes(str[i])) {
+            if (isNegationOp(str,i)) result += ` ${str[i]}`;
+            else result += ` ${str[i]} `;
+        } else {
+            result += str[i];
+        }
+    }
+
+    return result;
+}
+
+buttons.forEach(n => n.addEventListener("click", e => {
+    errorMsg.textContent = '';
+    try {
+        if (mode==='quick') quickCalc(e);
+        else if (mode==='expression') expressionCalc(e);
+
+    } catch (e) {
+        if (mode==='quick') {
+            errorMsg.textContent = `ERROR: ${e.message}`;
+            savedNum = '';
+            operator = '';
+            inputNum = '';
+            equalOperatorDisplay = '';
+            displayTextBot.textContent = 'ERROR';
+            displayTextTop.textContent = '';
+        }
+        else if (mode==='expression') {
+            errorMsg.textContent = `ERROR: ${e.message}`;
+            equalOperatorDisplay = '';
+            inputNum = '';
+        }
+    }
+}));
 
 
 
@@ -113,8 +251,8 @@ buttons.forEach(n => n.addEventListener("click", e => handleInput(e)));
 /** Returns true if character is an operator */
 const isOperator = char => ['√','!','^','%','÷','/','×','*','+','-'].includes(char);
 
-/** Returns true if it's a negative symbol before a number (not an operator) */
-const isNegativePrefix = (expr, i) => {
+/** Returns true if it is a negation operator */
+const isNegationOp = (expr, i) => {
     if (expr.length-1 < i+1) return false;
     return ( (i-1<0 || isOperator(expr[i-1])) && expr[i]==='-' && !isNaN(expr[i+1]) )
 }
@@ -132,6 +270,8 @@ const findMatchingParenthesis = (expr, start) => {
             if (count === 0) return i;
         }
     }
+
+    throw new Error('Invalid expression')
 };
 
 /** Performs a basic operations between two numbers */
@@ -139,7 +279,10 @@ function operate(a, b, op) {
     if (op==='+') return a+b;
     else if (op==='-') return a-b;
     else if (op==='×'||op==='*') return a*b;
-    else if (op==='÷'||op==='/') return a/b;
+    else if (op==='÷'||op==='/') {
+        if (b!==0) return a/b;
+        else throw new Error('Tried to divide by 0')
+    }
     else if (op==='%') return a%b;
     else if (op==='^') return a**b;
     else return NaN;
@@ -148,10 +291,16 @@ function operate(a, b, op) {
 
 /** Returns the factorial of a number */
 function factorial(n) {
-    if (n%1!==0) return NaN;
-    if (n < 0) return;
+    if (n%1!==0) throw new Error('Tried to find factorial of a decimal number');
+    if (n < 0) throw new Error('Tried to find factorial of a negative number');
     if (n < 2) return 1;
     return n * factorial(n - 1);
+}
+
+/** Calculates sqrt, throws error if recieves negative number */
+function getSqrt(n) {
+    if (n<0) throw new Error('Tried to find square root of a negative number')
+    return Math.sqrt(n);
 }
 
 //  CALCULATOR FUNCTIONS //
@@ -168,17 +317,17 @@ function exprToArray(expr) {
 
     while (i<l) {
         //if it is an operator, push to array
-        if (isOperator(expr[i]) && !isNegativePrefix(expr, i)) {
+        if (isOperator(expr[i]) && !isNegationOp(expr, i)) {
 
             exprArray.push(expr[i]);
             i++;
 
         } 
         //if it is a number (negative or not), find whole number and push
-        else if (!isNaN(expr[i]) || isNegativePrefix(expr, i)) { 
+        else if (!isNaN(expr[i]) || isNegationOp(expr, i)) { 
 
             let currentNum = '';
-            while (!isNaN(expr[i]) || expr[i]==='.' || isNegativePrefix(expr, i)) {
+            while (!isNaN(expr[i]) || expr[i]==='.' || isNegationOp(expr, i)) {
                 currentNum += expr[i];
                 i++;
             }
@@ -201,7 +350,7 @@ function exprToArray(expr) {
 
 /** Calculates an expression inside an array - Uses P-E-MD-AS, from left to right when ambiguous */
 function calculateExpression(arr) {
-    const operations = [['^'],['%','×','*','/','÷'],['+','-']];
+    const operations = [['^'],['×','*','/','÷'],['+','-']];
 
     //handles unary operations
     for (let i=0; i < arr.length; i++) {
@@ -209,7 +358,10 @@ function calculateExpression(arr) {
             arr.splice(i-1, 2, factorial(arr[i-1]));
         }
         else if (arr[i] === '√') {
-            arr.splice(i, 2, Math.sqrt(arr[i+1]));
+            arr.splice(i, 2, getSqrt(arr[i+1]));
+        }
+        else if (arr[i] === '%') {
+            arr.splice(i-1, 2, arr[i-1]/100);
         }
     }
 
